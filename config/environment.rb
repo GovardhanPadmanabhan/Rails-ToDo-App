c46a04d112fa7f131d819fd187481a48cb6d07bd
Govardhan Padmanabhan <itn.govardhan.p@grabtaxi.com>
2019-07-17 17:34:35 +0800

    Added ActionMailer

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+    :user_name => Credential.send_grid_username,
+    :password => Credential.send_grid_password,
+    :domain => 'serene-hollows-10623.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
