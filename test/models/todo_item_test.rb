8d55d299322c48fc84557b12acd27f05ffd50483
Govardhan Padmanabhan <itn.govardhan.p@grabtaxi.com>
2019-07-17 17:00:36 +0800

    Fixed Indentation

--- test/models/todo_item_test.rb
+++ test/models/todo_item_test.rb
@@ -2,13 +2,13 @@ require 'test_helper'
 
 class TodoItemTest < ActiveSupport::TestCase
   test "todo item" do
-     item = TodoItem.new :content => todo_items(:one).content,
-     :todo_list => todo_items(:one).todo_list,
-     :completed_at => todo_items(:one).completed_at
+    item = TodoItem.new :content => todo_items(:one).content,
+    :todo_list => todo_items(:one).todo_list,
+    :completed_at => todo_items(:one).completed_at
      
-      assert item.save
+    assert item.save
 
-     item_copy = TodoItem.find(item.id)
+    item_copy = TodoItem.find(item.id)
 
     assert_equal item.content, item_copy.content
 
