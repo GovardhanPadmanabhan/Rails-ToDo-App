25b7105adea30002fa536475e85105d871ab4e70
Govardhan Padmanabhan <itn.govardhan.p@grabtaxi.com>
2019-07-26 19:18:44 +0800

    Chnaged to rails-todo-staging-heroku.com

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,6 @@ module Todo
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.action_mailer.default_url_options = { host: 'agile-inlet-89346.herokuapp.com' }
+    config.action_mailer.default_url_options = { host: 'rails-todo-staging.herokuapp.com' }
   end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'Password Reset for ToDo App <passwords@serene-hollows-10623.herokuapp.com>'
+  config.mailer_sender = 'Password Reset for ToDo App <passwords@rails-todo-staging.herokuapp.com>'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
