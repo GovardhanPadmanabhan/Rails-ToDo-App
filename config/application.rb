234be2c192904c5b890ff1f6c4b40f8603e9d6c6
Govardhan Padmanabhan <itn.govardhan.p@grabtaxi.com>
2019-07-26 20:13:02 +0800

    forgot password

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,6 @@ module Todo
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.action_mailer.default_url_options = { host: 'rails-todo-staging.herokuapp.com' }
+    config.action_mailer.default_url_options = { host: 'http://rails-todo-staging.herokuapp.com' }
   end
 end
